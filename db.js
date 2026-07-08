f2a2f425797b41f113c61e3bc773b0d6a878552d
codewithshabbir@gmail.com <codewithshabbir@gmail.com>
2026-07-08 12:54:21 +0530

    test

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+console.log({
+  DB_HOST: process.env.DB_HOST,
+  DB_USER: process.env.DB_USER,
+  DB_NAME: process.env.DB_NAME,
+  DB_PASSWORD: process.env.DB_PASSWORD ? "exists" : "missing"
+});
 // Create a connection pool using the .env variables
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
