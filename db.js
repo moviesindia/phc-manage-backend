74268cc6ca6190340167643335b1226fd0b92479
codewithshabbir@gmail.com <codewithshabbir@gmail.com>
2026-07-08 12:51:12 +0530

    added port

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Create a connection pool using the .env variables
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
